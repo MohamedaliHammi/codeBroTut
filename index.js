1ebdfd84f01a3fafda49ce1ae1fc8a9c4778d799
Your Name <107193947+MohamedaliHammi@users.noreply.github.com>
2022-07-26 14:48:50 +0100

    second commit of the second part of the tut

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 console.log('ola');
+
+//*  ----counter----
+
+
+let count = 0;
+document.getElementById("decreasebtn").onclick = function () {
+    count -= 1;
+    document.getElementById('number').innerHTML = count;
+};
+document.getElementById("resetbtn").onclick =function () {
+    count = 0;
+   document.getElementById('number').innerHTML = count; 
+};
+
+document.getElementById("increasebtn").onclick = function () {
+    count += 1;
+    document.getElementById('number').innerText = count;
+};
+
+
+
+
+
+
+//* rolle the dice section 
+
+document.getElementById("roller").onclick = function(){
+document.getElementById("dice1").innerHTML = Math.floor(Math.random() * 6 + 1);
+
+document.getElementById("dice2").innerHTML = Math.floor(Math.random() * 6 + 1);
+
+document.getElementById("dice3").innerHTML = Math.floor(Math.random() * 6 + 1);
+
+   }
+
+
+
+
+
+
+
