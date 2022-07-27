d3fc0c0a7c2e44ee1259ae85108d02f40d38199c
Your Name <107193947+MohamedaliHammi@users.noreply.github.com>
2022-07-27 07:35:23 +0100

    fouth part of the tut

--- index.js
+++ index.js
@@ -90,6 +90,30 @@ let dicenum3 = Math.floor(Math.random() * 6 + 1);
  
 // our logic
 
+document.getElementById('convertButton').onclick = function (){
+let temp;
+
+if (document.getElementById('Cbutton').checked){
+    temp =document.getElementById('textBox').value
+    temp = Number(temp);
+    temp = toCelsius(temp);
+    document.getElementById('tempLabel').innerHTML = temp + "°C" // alt 0176
+
+}else if(document.getElementById('Fbutton').checked){
+    temp = document.getElementById('textBox').value
+    temp = Number(temp);
+    temp = toFrenheit(temp);
+    document.getElementById('tempLabel').innerHTML = temp + "°F"
+}else{
+  document.getElementById('tempLabel').innerHTML = 'select a unit'
+}
+
+
+}
+
+
+
+
     function toCelsius(temp){
         return (temp - 32) * (5 / 9);
     }
@@ -97,6 +121,10 @@ let dicenum3 = Math.floor(Math.random() * 6 + 1);
     function toFrenheit (temp){
         return temp * 9 / 5 + 32;
     }
+
+
+  // * Timer  (my try to finish later)
+
 
 
 
